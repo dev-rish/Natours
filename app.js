3ca5333fa69a9958e0d1e455b697abc0d048123a
Rishabh Singh <1997rishabhsingh@gmail.com>
2020-04-19 18:30:25 +0530

    Fix payment functionality

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const { globalErrorHandler } = require('./controllers/errorController');
+const { webhookCheckout } = require('./controllers/bookingController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -51,6 +52,13 @@ const limiter = rateLimit({
   message: 'Too many requests. Please try again later!'
 });
 app.use('/api', limiter);
+
+// This Route is placed here since stripe needs req in stream and not json hence its placed before express.json
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  webhookCheckout
+);
 
 // Body parser, read data from body into req.body and limit req body size
 app.use(express.json({ limit: '10kb' }));
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_API_KEY);
 
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -13,12 +14,14 @@ const getCheckoutSession = catchAsync(async (req, res) => {
   // Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // CAUTION: Not secure since url can be hit directly and hence the webhooks
-    success_url: `${req.protocol}://${req.get('host')}?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+    // NOTE: was just a temp. workaround until the site wasn't deployed. Not secure since url can be hit directly and hence the webhooks
+    // success_url: `${req.protocol}://${req.get('host')}?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-bookings`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
+    // Will be used in webhook
     client_reference_id: req.params.tourId,
     line_items: [
       {
@@ -39,16 +42,46 @@ const getCheckoutSession = catchAsync(async (req, res) => {
   });
 });
 
-const createBookingOnCheckout = catchAsync(async (req, res, next) => {
-  const { tour, user, price } = req.query;
-  // i.e. if homepage is after checkout
-  if (tour && user && price) {
-    await Booking.create({ tour, user, price });
-    res.redirect(req.originalUrl.split('?')[0]);
-  } else {
-    next();
+// NOTE: was just a temp. workaround until the site wasn't deployed
+// const createBookingOnCheckout = catchAsync(async (req, res, next) => {
+//   const { tour, user, price } = req.query;
+//   // i.e. if homepage is after checkout
+//   if (tour && user && price) {
+//     await Booking.create({ tour, user, price });
+//     res.redirect(req.originalUrl.split('?')[0]);
+//   } else {
+//     next();
+//   }
+// });
+
+const createBookingOnCheckout = async session => {
+  const tour = session.client_reference_id;
+  const user = (await User.find({ email: session.customer_email })).id;
+  const price = session.line_items[0].amount / 100;
+  await Booking.create({ tour, user, price });
+};
+
+const webhookCheckout = (req, res, next) => {
+  let event;
+  try {
+    const signature = req.header['stripe-signature'];
+    // Raw req.body
+    event = stripe.webhooks.constructEvent(
+      res.body,
+      signature,
+      process.env.STRIPE_SECRET_WEBHOOK
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
-});
+
+  // Just to be sure
+  if (event.type === 'checkout.session.completed') {
+    this.createBookingOnCheckout(event.data.object);
+  }
+
+  res.status(200).json({ received: true });
+};
 
 const createBooking = factory.createOne(Booking);
 const getBooking = factory.getOne(Booking);
@@ -63,5 +96,6 @@ module.exports = {
   updateBooking,
   deleteBooking,
   getCheckoutSession,
-  createBookingOnCheckout
+  createBookingOnCheckout,
+  webhookCheckout
 };
